813c164cf4b6b837fbfd81508eb2c4e805e2a41b
Akwasi Appiadjei Asante-Krobea <49639685+Akwasi123@users.noreply.github.com>
2021-03-08 15:42:35 +0000

    added.php extension

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -281,7 +283,7 @@
             </div>
         </section>
     </main>
-
+    ?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
